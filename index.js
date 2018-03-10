dd2fd2247bc83be26f8931ec051d4ff27d0d5313
Zach Dorsch <zachary.dorsch@gmail.com>
2018-03-10 15:45:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
 
+function holidayCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`
 
 happyHolidays()
 happyHolidaysTo("you")
 happyHolidayTo("Independence day","zach")
+holidayCountdown(10,"Christmas")
